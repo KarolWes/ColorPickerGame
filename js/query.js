08bb2fefc58b29729f7162b4417c5ed86432c650
KarolWes <karol.wesolowski01@gmail.com>
2023-09-03 21:19:20 +0200

    final

--- js/query.js
+++ js/query.js
@@ -3,16 +3,25 @@ let COLS = ROWS;
 var colorArray = [];
 
 function randomColor(){
-    return '#'+((1<<24)*Math.random() | 0).toString(16);
+    let r=Math.floor(Math.random()*256);
+    let g=Math.floor(Math.random()*256);
+    let b=Math.floor(Math.random()*256);
+    // return '#'+((1<<24)*Math.random() | 0).toString(16);
+    return 'rgb('+r+','+g+','+b+')';
 }
+
+function addNewColor(){
+    var trackColor = randomColor();
+    colorArray.push(trackColor);
+    return trackColor;
+}
+
 function makeBoard() {
     var html = '';
     for(var row = 0; row < ROWS; row++){
         html += '<div class="row">';
         for (var col = 0; col < COLS; col++){
-            var trackColor = randomColor();
-            colorArray.push(trackColor);
-            html += '<div class="cell" style="background-color:'+trackColor+'">0</div>';
+            html += '<div class="cell" style="background-color:'+ addNewColor() +'">0</div>';
         }
         html += '</div>';
     }
@@ -29,15 +38,27 @@ function makeBoard() {
         display: 'inline-block'
     })
 }
-function checkCell(){
-    console.log($(this).css('backgroundColor'));
-}
-
 function pickMyColor() {
     let newColor = colorArray.splice(Math.floor(Math.random() * colorArray.length),1);
     $('#findme').css({
         backgroundColor: newColor
     });
+}
+
+
+
+function checkCell(){
+    let currentColor = $(this).css('backgroundColor');
+    if (currentColor === $('#findme').css('backgroundColor')){
+        var countValue = Number($(this).text());
+        countValue ++;
+        $(this).text(countValue);
+        $(this).css('backgroundColor', addNewColor());
+        pickMyColor();
+        $('#message').html('Correct, great');
+    }else{
+        $('#message').html('Wrong, unfortunately');
+    }
 }
 
 function startGame(){
